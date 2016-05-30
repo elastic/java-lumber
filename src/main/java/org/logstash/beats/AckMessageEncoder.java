d48429522aa713b51c4e42d6871d259296ab02cd
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-05-30 14:09:12 -0400

    Adding a batch encoder for testing purpose and also to be able to write a client.

---
+++ src/main/java/org/logstash/beats/AckMessageEncoder.java
@@ -0,0 +1,9 @@
+package org.logstash.beats;
+
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Created by ph on 2016-05-30.
+ */
+public class AckMessageEncoder extends MessageToByteEncoder<AckMessage> {
+}
---
+++ src/main/java/org/logstash/beats/KeepAliveMessage.java
@@ -0,0 +1,7 @@
+package org.logstash.beats;
+
+/**
+ * Created by ph on 2016-05-30.
+ */
+public class KeepAliveMessage {
+}
